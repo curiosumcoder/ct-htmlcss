cdbb58e89d046419b36178d2cf9ef405eb90f683
Gilberto Bermúdez Garro <gbermude@outlook.com>
2024-08-20 11:05:46 -0600

    Update

---
+++ 20240820/ndemo8/dist/main.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+const PORT = 3030;
+const app = express();
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
+app.get('/', (request, response) => {
+    //response.status(200).send('Hello World');
+    response.render('home/index');
+});
+app.listen(PORT, () => { console.log('Server running at PORT: ', PORT); });
---
+++ 20240820/ndemo8/src/main.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import { engine } from 'express-handlebars';
+
+const PORT = 3030;
+
+const app = express();
+
+app.engine('handlebars',engine())
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
+
+app.get('/', (request: Request, response: Response) => {
+    //response.status(200).send('Hello World');
+    response.render('home/index')
+})
+
+app.listen(PORT, () => {console.log('Server running at PORT: ', PORT);})
